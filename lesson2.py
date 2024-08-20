a6c037dba64f596b8aef0b9b5fa0e58be44646f1
orshanskayasofia <orshanskayasofia@gmail.com>
2024-08-20 17:00:25 +0300

    Add files via upload

---
+++ lesson2.py
@@ -0,0 +1,5 @@
+number_of_homeworks = 12
+number_of_hours = 1.5
+course_name = 'Python'
+time_for_one_task = (number_of_hours / number_of_homeworks)
+print ('Курс:', str(course_name) + ',', 'всего задач:', str(number_of_homeworks) + ',', 'затрачено часов:', str(number_of_hours) + ',', 'среднее время выполнения:', time_for_one_task)
